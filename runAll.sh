e1f540d4527e417c9d044a04a5e8968fef684b89
Dong Meng <mengdong0427@gmail.com>
2017-04-06 11:25:57 -0700

    auto

--- runAll.sh
+++ runAll.sh
@@ -110,6 +110,36 @@ echo "This container IP : ${IP}"
 
 sleep 60
 
+chown mapr:mapr /var/log/opentsdb
+cp /etc/opentsdb/opentsdb.conf /etc/opentsdb/opentsdb.conf.orig
+
+cat <<EOF > /etc/opentsdb/opentsdb.conf
+tsd.network.port = 4242
+tsd.http.staticroot = /usr/share/opentsdb/static/
+tsd.http.cachedir = /tmp/opentsdb
+tsd.core.plugin_path = /usr/share/opentsdb/plugins
+tsd.storage.enable_compaction = true
+# MapR-DB does not utilize this value, but it must be set to something
+tsd.storage.hbase.zk_quorum = localhost:5181
+tsd.storage.hbase.data_table = $TBL_PATH/tsdb
+tsd.storage.hbase.uid_table = $TBL_PATH/tsdb-uid
+tsd.storage.hbase.meta_table = $TBL_PATH/tsdb-meta
+tsd.storage.hbase.tree_table = $TBL_PATH/tsdb-tree
+tsd.storage.fix_duplicates = true
+tsd.core.auto_create_metrics = true
+tsd.http.request.enable_chunked = true
+EOF
+
+mkdir /root/opentsdb && cd /root/opentsdb/ &&  git clone https://github.com/mengdong/opentsdb-mapr.git
+/root/opentsdb/opentsdb-mapr/install.sh
+sed -i -e "s/^TABLES_PATH=.*$/TABLES_PATH=\"$TBL_PATH_E\"/" /root/opentsdb/opentsdb-mapr/create_table.sh
+cp /root/opentsdb/opentsdb-mapr/create_table.sh /tmp/.
+su mapr -c /tmp/create_table.sh
+sleep 5
+
+chkconfig opentsdb on
+service opentsdb start
+
 cp /home/mapr/maprts-docker-jars/spark-streaming-kafka-0-9_2.11-2.0.1-mapr-1611.jar /opt/mapr/spark/spark-2.0.1/jars/.
 sh /opt/mapr/spark/spark-2.0.1/sbin/stop-master.sh
 sh /opt/mapr/spark/spark-2.0.1/sbin/start-master.sh
